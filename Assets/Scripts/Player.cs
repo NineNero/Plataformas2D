216207fb3e7071f47dc18f8e3dbd439ed4927f89
NineNero <salmaurbano01@gmail.com>
2023-09-20 09:35:36 +0200

    rBody

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,14 @@ public class Player : MonoBehaviour
 {
     [SerializeField]private float _playerSpeed = 5;
     private float _playerInputH;
-    private float _playerInputV;
+    //private float _playerInputV;
+
+    private Rigidbody2D _rBody2D;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,11 +22,16 @@ public class Player : MonoBehaviour
         PlayerMovement();
     }
 
+    void FixedUpdate()
+    {
+        _rBody2D.AddForce(new Vector2(_playerInputH * _playerSpeed, 0));
+    }
+
     void PlayerMovement()
     {
         _playerInputH = Input.GetAxis("Horizontal");
-        _playerInputV = Input.GetAxis("Vertical");
+        /*_playerInputV = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector2(_playerInputH, _playerInputV) * _playerSpeed * Time.deltaTime);
+        transform.Translate(new Vector2(_playerInputH, _playerInputV) * _playerSpeed * Time.deltaTime);*/
     }
 }
