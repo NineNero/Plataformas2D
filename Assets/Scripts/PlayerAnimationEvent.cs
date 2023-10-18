3f8d24a2c826f4fe519ec1f456398a6b012d6c0f
NineNero <salmaurbano01@gmail.com>
2023-10-18 10:45:53 +0200

    EditSignal

---
+++ Assets/Scripts/PlayerAnimationEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationEvent : MonoBehaviour
+{
+    public void SignalTest(string texto)
+    {
+        Debug.Log(texto);
+    }
+}
